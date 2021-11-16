47ef07e890ea220689a69322c85f03e20b51e35e
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-11-16 16:19:44 -0600

    feat(fem/rxjs-fundamentals): loading states

--- applications/loading-states/script.js
+++ applications/loading-states/script.js
@@ -10,3 +10,28 @@ import {
   form,
   fetchData,
 } from './utilities';
+
+const loading$ = fromEvent(form, 'submit').pipe(
+  exhaustMap(() => {
+    const data$ = fetchData().pipe(shareReplay(1));
+    const showLoading$ = of(true).pipe(
+      delay(+showLoadingAfterField.value),
+      tap(() => showLoading(true)),
+    );
+
+    const timeToHideTheLoading$ = timer(+showLoadingForAtLeastField.value).pipe(
+      first(),
+      // tap(() => showLoading(false)),
+    );
+
+    const shouldShowLoading$ = concat(
+      showLoading$,
+      timeToHideTheLoading$,
+      data$.pipe(tap(() => showLoading(false))),
+    );
+
+    return race(data$, shouldShowLoading$);
+  }),
+);
+
+loading$.subscribe();
--- applications/pokemon/script.js
+++ applications/pokemon/script.js
@@ -14,6 +14,8 @@ import {
   concat,
   take,
   EMPTY,
+  pluck,
+  first,
 } from 'rxjs';
 
 import { fromFetch } from 'rxjs/fetch';
@@ -25,6 +27,54 @@ import {
   endpointFor,
   search,
   form,
+  renderPokemon,
 } from '../pokemon/utilities';
 
-const endpoint = 'http://localhost:3333/api/pokemon?delay=100';
+const endpoint = 'http://localhost:3333/api/pokemon/';
+
+const searchPokemon = (searchTerm) => {
+  return fromFetch(endpoint + 'search/' + searchTerm).pipe(
+    mergeMap((response) => response.json()),
+  );
+};
+
+const getPokemonData = (pokemon) => {
+  return fromFetch(endpoint + pokemon.id).pipe(
+    mergeMap((response) => response.json()),
+  );
+};
+
+const search$ = fromEvent(form, 'submit').pipe(
+  map(() => search.value),
+  switchMap(searchPokemon),
+  pluck('pokemon'),
+  mergeMap((pokemon) => pokemon),
+  first(),
+  switchMap((pokemon) => {
+    const pokemon$ = of(pokemon);
+    const additionalData$ = getPokemonData(pokemon).pipe(
+      map((data) => ({
+        ...pokemon,
+        data,
+      })),
+    );
+    return merge(pokemon$, additionalData$);
+  }),
+  tap(renderPokemon),
+);
+
+const oldSearch$ = fromEvent(search, 'input').pipe(
+  debounceTime(300),
+  map((event) => event.target.value),
+  distinctUntilChanged(),
+  switchMap((searchTerm) => {
+    return fromFetch(endpoint + searchTerm + '?delay=100&chaos=true').pipe(
+      mergeMap((response) => response.json()),
+    );
+  }),
+  tap(clearResults),
+  pluck('pokemon'),
+  tap(addResults),
+);
+
+search$.subscribe(console.log);
