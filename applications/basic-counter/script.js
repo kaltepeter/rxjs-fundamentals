b224330108555eae435679c7328baa10ce5169f6
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-11-16 11:12:41 -0600

    feat(fem/rxjs-fundamentals): basic counter excercise

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -3,3 +3,16 @@ import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
+
+let interval$ = interval(1000);
+let subscription;
+
+start$.subscribe((v) => {
+  console.log('Start');
+  subscription = interval$.subscribe(setCount);
+});
+
+pause$.subscribe((v) => {
+  console.log('pause');
+  subscription.unsubscribe();
+});
