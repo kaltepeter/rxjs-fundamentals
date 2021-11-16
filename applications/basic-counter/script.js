305a1954cc3e4e027cea6c4863b1eb7178fa45f9
Kayla Altepeter <kayla@kaylaaltepeter.com>
2021-11-16 12:18:39 -0600

    feat(fem/rxjs-fundamentals): basic counter improved

--- applications/basic-counter/script.js
+++ applications/basic-counter/script.js
@@ -1,18 +1,36 @@
-import { fromEvent, interval, merge, NEVER } from 'rxjs';
+import {
+  fromEvent,
+  interval,
+  merge,
+  NEVER,
+  skipUntil,
+  takeUntil,
+  scan,
+} from 'rxjs';
 import { setCount, startButton, pauseButton } from './utilities';
 
 const start$ = fromEvent(startButton, 'click');
 const pause$ = fromEvent(pauseButton, 'click');
 
-let interval$ = interval(1000);
-let subscription;
+// let interval$ = interval(1000);
+// let subscription;
 
-start$.subscribe((v) => {
-  console.log('Start');
-  subscription = interval$.subscribe(setCount);
-});
+// start$.subscribe((v) => {
+//   console.log('Start');
+//   subscription = interval$.subscribe(setCount);
+// });
 
-pause$.subscribe((v) => {
-  console.log('pause');
-  subscription.unsubscribe();
-});
+// pause$.subscribe((v) => {
+//   console.log('pause');
+//   subscription.unsubscribe();
+// });
+
+// interval$.pipe(skipUntil(start$), takeUntil(pause$)).subscribe(setCount);
+// const counter$ = interval(1000).pipe(skipUntil(start$), takeUntil(pause$));
+const counter$ = interval(1000).pipe(
+  skipUntil(start$),
+  scan((state) => state + 1, 0),
+  takeUntil(pause$),
+);
+
+counter$.subscribe(setCount);
